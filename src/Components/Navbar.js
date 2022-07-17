f7b4dfb437894c71860af17b76e62f96f6d47d4d
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-07-17 21:45:50 +1000

    Margin Change

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import "../Styles/Navbar.css";
-import logo from "../Images/smallLogo.png"
+import logo from "../Images/logo.png"
 import {Link} from 'react-router-dom';
 
 function Navbar() {
   return (
     <div className='navbar'>
         <img className="logo" src={logo} alt="logo"/>
+        <div className='name-container'>
+            <p className='title-name'>Jonty Atkinson</p>
+            <p className='title-undertext'>Personal Portfolio</p>
+        </div>
         <div className='toggle-button'>
             
         </div>
         <div className='links'>
-            <Link to="/">Home</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/resume">Resume</Link>
+            <Link to="/" className='hover-animation'>Home</Link>
+            <Link to="/projects" className='hover-animation'>Projects</Link>
+            <Link to="/resume" className='hover-animation'>Resume</Link>
         </div>
     </div>
   )
