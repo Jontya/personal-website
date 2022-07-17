73eb036b01c2ceb4357f18ea3b2bc3581a2eeed7
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-07-17 21:45:24 +1000

    Began Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import "../Styles/Footer.css";
+import logo from "../Images/logo.png";
+import { IconContext } from 'react-icons';
+import {FaGithub, FaLinkedin, FaEnvelope} from "react-icons/fa";
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <div className='footer-contacts'>
+      <IconContext.Provider value ={{size: "2em", }}>
+        <ul className='footer-list'>
+            <li className='footer-item'><a href="mailto:jontyatkinson2002@gmail.com"> <FaEnvelope/> Email</a></li>
+            <li className='footer-item'><a href="https://www.linkedin.com/in/jonty-atkinson/"><FaLinkedin/> LinkedIn</a></li>
+            <li className='footer-item'><a href="https://github.com/Jontya"><FaGithub/> GitHub</a></li>
+          </ul>
+      </IconContext.Provider>
+      </div>
+
+    </div>
   )
 }
 
