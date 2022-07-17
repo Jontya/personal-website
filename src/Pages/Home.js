78556dce01e7a983dfbcb2c8e275e32015b0042a
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-07-17 21:45:37 +1000

    Updated Layout

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Sidebar from '../Components/Sidebar.js'
+import "../Styles/Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home-container">
+      <Sidebar/>
+      <div className='about-me'>
+        
+      </div>
+      <div className='project-showcase'>
+
+
+      </div>
+    </div>
   )
 }
 
