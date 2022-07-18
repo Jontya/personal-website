cafcf7ded87100bbcbf1c261d85eed799a9c484d
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-07-18 20:45:55 +1000

    Updated Footer Content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,17 +7,22 @@ import {FaGithub, FaLinkedin, FaEnvelope} from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className='footer-container'>
-      <div className='footer-contacts'>
-      <IconContext.Provider value ={{size: "2em", }}>
-        <ul className='footer-list'>
-            <li className='footer-item'><a href="mailto:jontyatkinson2002@gmail.com"> <FaEnvelope/> Email</a></li>
-            <li className='footer-item'><a href="https://www.linkedin.com/in/jonty-atkinson/"><FaLinkedin/> LinkedIn</a></li>
-            <li className='footer-item'><a href="https://github.com/Jontya"><FaGithub/> GitHub</a></li>
+    <div className='footer'>
+      <div className='footer-container'>
+        <div className='footer-contacts'>
+        <IconContext.Provider value ={{size: "1.2em"}}>
+          <ul className='footer-list'>
+            <li>CONTACT ME:</li>
+            <li className='footer-item'><a href="mailto:jontyatkinson2002@gmail.com"> <FaEnvelope className='icon'/> EMAIL</a></li>
+            <li className='footer-item'><a href="https://www.linkedin.com/in/jonty-atkinson/"><FaLinkedin className='icon'/> LINKEDIN</a></li>
+            <li className='footer-item'><a href="https://github.com/Jontya"><FaGithub className='icon'/> GITHUB</a></li>
           </ul>
-      </IconContext.Provider>
+        </IconContext.Provider>
+        </div>
+        <div className='footer-stamp'>
+          <p>&copy; 2022 Jonty Atkinson</p>
+        </div>
       </div>
-
     </div>
   )
 }
