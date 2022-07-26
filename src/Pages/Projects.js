35c00d2067d512b0f272f57be348506c4f601b2d
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-07-26 16:21:58 +1000

    Basic Layout

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../Styles/Projects.css'
+import SortingVisualizer from '../Projects/SortingVisualizer/SortingVisualizer'
+
+import logo from '../Images/logo.png'
+
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+      <div className='project-item'>
+        
+      </div>
+    </div>
   )
 }
 
