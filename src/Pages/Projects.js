a387cdc85b0b29946db952a8672ae5488b660b46
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-11-22 23:44:55 +1100

    began refactoring the project page

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,33 +1,45 @@
 import React from 'react'
 import '../Styles/Projects.css'
-import SortingPreview from '../Images/sortingPreview.PNG'
-import ProductLineSim from '../Images/productLineSim.PNG'
-
 
 function Projects() {
-    return (
-        <div className='projects'>
-            <div className='project-item'>
-                <img className="project-preview" src={ProductLineSim} alt="project-preview"/>
-                <p className='project-title'>Production Line Discrete Event Simulator</p>
-                <p className='project-description'>Project 1</p>
-                <button className='project-button'>GitHub Repo</button><button className='project-button-2'>Try It Out</button>
-            </div>
-
-            <div className='project-item'>
-                <img className="project-preview" src={SortingPreview} alt="sorting-preview"/>
-                <p className='project-title'>Sorting Algorithm Visualizer</p>
-                <p className='project-description'>Project 2</p>
-                <button className='project-button'>GitHub Repo</button><button className='project-button-2'>Try It Out</button>
-            </div>
-
-            <div className='project-item'>
-                <img className="project-preview" src={ProductLineSim} alt="project-preview"/>
-                <p className='project-title'>Scheduling Algorithm Simulator</p>
-                <p className='project-description'>Project 3</p>
-                <button className='project-button'>GitHub Repo</button><button className='project-button-2'>Try It Out</button>
-            </div>
+  return (
+      <div className='project-container'>
+        <div className='project-block'>
+          <span className='project-heading'>Sorting Algorithm Visualizer</span>
+          <p className='project-desc'>
+            Description
+          </p>
+          <button className='project-git-button'>GitHub Repo</button>
+          <button className='project-try-button'>Try It Out</button>
         </div>
+
+        <div className='project-block'>
+          <span className='project-heading'>Production Line Discrete Event Simulator</span>
+          <p className='project-desc'>
+            Description
+          </p>
+          <button className='project-git-button'>GitHub Repo</button>
+          <button className='project-try-button'>Try It Out</button>
+        </div>
+
+        <div className='project-block'>
+          <span className='project-heading'>Program Planner</span>
+          <p className='project-desc'>
+            Description
+          </p>
+          <button className='project-git-button'>GitHub Repo</button>
+          <button className='project-try-button'>Try It Out</button>
+        </div>
+
+        <div className='project-block'>
+          <span className='project-heading'>Scheduling Algorithm Simulator</span>
+          <p className='project-desc'>
+            Description
+          </p>
+          <button className='project-git-button'>GitHub Repo</button>
+          <button className='project-try-button'>Try It Out</button>
+        </div>
+      </div>
     );
 }
 
